f88e8180655b5236c13cec1773f1fddb598f9567
Julius Philip Bartolome <julius.bartolome@inriver.com>
2024-01-21 21:45:23 +0800

    Refactor code and remove unused models
    The codebase has been refactored to improve readability and efficiency. As part of these changes, unnecessary models (`BackorderAvailabilityDetails`, `BackorderInventoryAllocation`, and `ItemLocationKey`) were removed. Additionally, methods that were cluttering the `LinnworksMacro` class have been broken out for better

--- Linnworks/src/netcore/OrderItemStockLocationAssignment/LinnworksMacro.cs
+++ Linnworks/src/netcore/OrderItemStockLocationAssignment/LinnworksMacro.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using LinnworksAPI;
-using OrderItemStockLocationAssignment.Models;
 
 namespace OrderItemStockLocationAssignment
 {
@@ -14,13 +13,13 @@ namespace OrderItemStockLocationAssignment
             try
             {
                 Logger.WriteInfo("Validating input parameters");
-                if (orderIds == null || orderIds?.Length == 0)
+                if (orderIds == null || orderIds.Length == 0)
                 {
                     Logger.WriteInfo("No orders provided, skipping macro");
                     return;
                 }
                 
-                if (alternateLocationIds == null || alternateLocationIds?.Length == 0)
+                if (alternateLocationIds == null || alternateLocationIds.Length == 0)
                 {
                     Logger.WriteInfo("No alternate locations provided, skipping macro");
                     return;
@@ -69,47 +68,53 @@ namespace OrderItemStockLocationAssignment
                 Logger.WriteInfo("Creating new orders based on allocations");
                 foreach (var locationId in backorderInventoryAllocation.LocationIds)
                 {
-                    Logger.WriteInfo($"Creating new order for location: {locationId}");
-                    var newOrder = Api.Orders.CreateNewOrder(locationId, false);
-
-                    newOrder.GeneralInfo.Status = order.GeneralInfo.Status;
-                    newOrder.GeneralInfo.Source = order.GeneralInfo.Source;
-                    newOrder.GeneralInfo.SubSource = order.GeneralInfo.SubSource;
-
-                    var updateOrderShippingInfoRequest = new UpdateOrderShippingInfoRequest
-                    {
-                        PostalServiceId = order.ShippingInfo.PostalServiceId,
-                        ManualAdjust = false
-                    };
-
-                    Api.Orders.SetOrderGeneralInfo(newOrder.OrderId, newOrder.GeneralInfo, false);
-                    Api.Orders.SetOrderShippingInfo(newOrder.OrderId, updateOrderShippingInfoRequest);
-                    Api.Orders.SetOrderCustomerInfo(newOrder.OrderId, order.CustomerInfo, false);
-
-                    var allocatedOrderItems = backorderInventoryAllocation.GetItemAllocationsByLocationId(locationId);
-                    foreach (var allocatedOrderItem in allocatedOrderItems)
-                    {
-                        var itemId = allocatedOrderItem.Key;
-                        var quantity = allocatedOrderItem.Value;
-
-                        var orderItem = backorderInventoryAllocation.GetOrderItem(itemId);
-                        var linePricingRequest = new LinePricingRequest
-                        {
-                            DiscountPercentage = orderItem.Discount,
-                            PricePerUnit = orderItem.PricePerUnit,
-                            TaxInclusive = orderItem.TaxCostInclusive,
-                            TaxRatePercentage = orderItem.TaxRate
-                        };
-
-                        Api.Orders.AddOrderItem(newOrder.OrderId, itemId, orderItem.ChannelSKU, locationId, quantity,
-                            linePricingRequest);
-                    }
-
-                    Logger.WriteInfo($"Finished creating new order for location: {locationId} with Order Id: {newOrder.OrderId}");
-                    Api.ProcessedOrders.AddOrderNote(orderId, $"Created new order {newOrder.OrderId} to reallocate backorders", true);
-                    Api.ProcessedOrders.AddOrderNote(newOrder.OrderId, $"This order is reallocated based on order {orderId}", true);
+                    CreateAlternateLocationOrder(locationId, order, backorderInventoryAllocation);
                 }
             }
+        }
+
+        private void CreateAlternateLocationOrder(Guid locationId, OrderDetails order,
+            BackorderInventoryAllocation backorderInventoryAllocation)
+        {
+            Logger.WriteInfo($"Creating new order for location: {locationId}");
+            var newOrder = Api.Orders.CreateNewOrder(locationId, false);
+
+            newOrder.GeneralInfo.Status = order.GeneralInfo.Status;
+            newOrder.GeneralInfo.Source = order.GeneralInfo.Source;
+            newOrder.GeneralInfo.SubSource = order.GeneralInfo.SubSource;
+
+            var updateOrderShippingInfoRequest = new UpdateOrderShippingInfoRequest
+            {
+                PostalServiceId = order.ShippingInfo.PostalServiceId,
+                ManualAdjust = false
+            };
+
+            Api.Orders.SetOrderGeneralInfo(newOrder.OrderId, newOrder.GeneralInfo, false);
+            Api.Orders.SetOrderShippingInfo(newOrder.OrderId, updateOrderShippingInfoRequest);
+            Api.Orders.SetOrderCustomerInfo(newOrder.OrderId, order.CustomerInfo, false);
+
+            var allocatedOrderItems = backorderInventoryAllocation.GetItemAllocationsByLocationId(locationId);
+            foreach (var allocatedOrderItem in allocatedOrderItems)
+            {
+                var itemId = allocatedOrderItem.Key;
+                var quantity = allocatedOrderItem.Value;
+
+                var orderItem = backorderInventoryAllocation.GetOrderItem(itemId);
+                var linePricingRequest = new LinePricingRequest
+                {
+                    DiscountPercentage = orderItem.Discount,
+                    PricePerUnit = orderItem.PricePerUnit,
+                    TaxInclusive = orderItem.TaxCostInclusive,
+                    TaxRatePercentage = orderItem.TaxRate
+                };
+
+                Api.Orders.AddOrderItem(newOrder.OrderId, itemId, orderItem.ChannelSKU, locationId, quantity,
+                    linePricingRequest);
+            }
+
+            Logger.WriteInfo($"Finished creating new order for location: {locationId} with Order Id: {newOrder.OrderId}");
+            Api.ProcessedOrders.AddOrderNote(order.OrderId, $"Created new order {newOrder.OrderId} to reallocate backorders", true);
+            Api.ProcessedOrders.AddOrderNote(newOrder.OrderId, $"This order is reallocated based on order {order.OrderId}", true);
         }
 
         private void UpdateOrderItemsQuantityBasedOnAllocations(BackorderInventoryAllocation backorderInventoryAllocation,
@@ -233,5 +238,104 @@ namespace OrderItemStockLocationAssignment
             Logger.WriteInfo($"Completed generation of Backorder Availability details for {result.Count} items.");
             return result;
         }
+    }
+
+    public class BackorderAvailabilityDetails
+    {
+        public BackorderAvailabilityDetails(int backorderQuantity, int[] alternateLocationQuantity)
+        {
+            if (backorderQuantity >= 0)
+            {
+                throw new ArgumentException("Backorder quantity must be negative");
+            }
+
+            Quantity = Math.Abs(backorderQuantity);
+            AlternateLocationAvailableQuantity = alternateLocationQuantity;
+        }
+
+
+        public int Quantity { get; }
+        public int[] AlternateLocationAvailableQuantity { get; }
+    }
+
+    public class BackorderInventoryAllocation
+    {
+        private readonly OrderDetails _order;
+        private readonly Dictionary<ItemLocationKey, int> _allocationQuantityByLocationAndItem;
+        private readonly Dictionary<Guid, OrderItem> _orderItemsById = new Dictionary<Guid, OrderItem>();
+
+        public BackorderInventoryAllocation(OrderDetails order)
+        {
+            OrderId = order.OrderId;
+            _order = order;
+            _allocationQuantityByLocationAndItem = new Dictionary<ItemLocationKey, int>();
+        }
+
+        public void AddAllocationByItem(OrderItem orderItem, IReadOnlyDictionary<Guid, int> quantityByLocationId)
+        {
+            if (!_orderItemsById.ContainsKey(orderItem.StockItemId))
+            {
+                _orderItemsById.Add(orderItem.StockItemId, orderItem);
+            }
+
+            foreach (var allocation in quantityByLocationId)
+            {
+                AddAllocation(orderItem.StockItemId, allocation.Key, allocation.Value);
+            }
+        }
+
+        public int GetAllocatedQuantity(Guid itemId)
+        {
+            return AllocationQuantityByLocationAndItem
+                .Where(a => a.Key.ItemId == itemId)
+                .Sum(a => a.Value);
+        }
+
+        public Dictionary<Guid, int> GetItemAllocationsByLocationId(Guid locationId)
+        {
+            return AllocationQuantityByLocationAndItem
+                .Where(a => a.Key.LocationId == locationId)
+                .ToDictionary(a => a.Key.ItemId, a => a.Value);
+        }
+
+        public OrderItem GetOrderItem(Guid itemId)
+        {
+            return _orderItemsById[itemId];
+        }
+
+        public Guid OrderId { get; }
+        public OrderDetails Order => _order;
+        public string OrderSource => _order.GeneralInfo.Source;
+        public string OrderSubSource => _order.GeneralInfo.SubSource;
+        public Guid FulfilmentLocationId => _order.FulfilmentLocationId;
+
+        public IReadOnlyDictionary<ItemLocationKey, int> AllocationQuantityByLocationAndItem =>
+            _allocationQuantityByLocationAndItem;
+
+        public IReadOnlyCollection<OrderItem> OrderItems => _orderItemsById.Values;
+
+        public IReadOnlyCollection<Guid> LocationIds => AllocationQuantityByLocationAndItem
+            .Select(a => a.Key.LocationId)
+            .Distinct()
+            .ToList();
+
+        private void AddAllocation(Guid itemId, Guid locationId, int quantity)
+        {
+            var key = new ItemLocationKey {ItemId = itemId, LocationId = locationId};
+            if (_allocationQuantityByLocationAndItem.ContainsKey(key))
+            {
+                _allocationQuantityByLocationAndItem[key] += quantity;
+            }
+            else
+            {
+                _allocationQuantityByLocationAndItem.Add(key, quantity);
+            }
+        }
+    }
+
+    public struct ItemLocationKey
+    {
+        public Guid ItemId { get; set; }
+        public Guid LocationId { get; set; }
     }
 }
